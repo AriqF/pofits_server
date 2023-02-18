b597a7ed140ff9e7d2fe5f60ce93768d75f233b6
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-02-18 20:10:55 +0700

    add success and error message for data

---
+++ src/utils/global/enum/success-message.enum.ts
@@ -0,0 +1,8 @@
+
+
+export enum DataSuccessID {
+    DataAdded = "Data berhasil ditambahkan",
+    DataUpdated = "Data berhasil disimpan",
+    DataDeleted = "Data berhasil dihapus",
+    DataHardDeleted = "Data berhasil dihapus secara permanen"
+}
