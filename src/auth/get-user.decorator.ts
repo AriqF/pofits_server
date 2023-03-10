2ab69b9727321e35c0d0fca3a769aa4bf939aad7
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-03-10 10:57:46 +0700

    fix user decorator refresh token

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -3,6 +3,6 @@ import { User } from "src/user/entities/user.entity";
 
 export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => {
     const req = ctx.switchToHttp().getRequest();
-    delete req.user.refresh_token;
+    // delete req.user.refresh_token;
     return req.user;
 })
