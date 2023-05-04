320d2e4e85c8527f86053ba28df15dfebc3f0ac8
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-05-04 10:52:56 +0700

    implement OOP in expenses and incomes entity

--- src/income-transaction/entities/income-transaction.entity.ts
+++ src/income-transaction/entities/income-transaction.entity.ts
@@ -1,44 +1,13 @@
 import { IncomeCategory } from "src/income-category/entities/income-category.entity";
+import { BaseTransactionEntity } from "src/transaction/entities/base-transaction.entity";
 import { User } from "src/user/entities/user.entity";
 import { Wallet } from "src/wallet/entities/wallet.entity";
 import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 
 @Entity("income_transactions")
-export class IncomeTransaction {
-    @PrimaryGeneratedColumn()
-    id: number;
-
-    @Column({ type: "bigint" })
-    amount: number;
-
+export class IncomeTransaction extends BaseTransactionEntity {
     @ManyToOne(() => IncomeCategory, category => category.id, { eager: true, onDelete: "CASCADE" })
     @JoinColumn({ name: "category" })
     category: IncomeCategory;
-
-    @Column({ type: "date" })
-    date: Date;
-
-    @Column({ length: 75 })
-    title: string;
-
-    @Column({ length: 255, nullable: true })
-    description: string;
-
-    @ManyToOne(() => Wallet, wallet => wallet.id, { eager: true })
-    @JoinColumn({ name: "wallet" })
-    wallet: Wallet;
-
-    @ManyToOne(() => User, user => user.id, { eager: true })
-    @JoinColumn({ name: "created_by" })
-    created_by: User;
-
-    @CreateDateColumn()
-    created_at: Date;
-
-    @UpdateDateColumn()
-    updated_at: Date;
-
-    @DeleteDateColumn()
-    deleted_at: Date;
 }
