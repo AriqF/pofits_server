3ea8a663670d53ea1cdd7c0d362568696f1187c9
Ariq Fachry R <ariqfachry2611@gmail.com>
2023-05-25 16:44:38 +0700

    exclude logging middleware for GET method

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -7,23 +7,26 @@ import { convertV6toV4, isIpv6 } from "src/utils/helper";
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        console.log({
-            timestamp: moment.tz("Asia/Jakarta").format(),
-            method: req.method,
-            path: req.path,
-            ip: isIpv6(req.ip) ? convertV6toV4(req.ip) : req.ip,
-            payload: {
-                body: {
-                    ...req.body
-                },
-                query: {
-                    ...req.query
-                },
-                params: {
-                    ...req.params
+        if (req.method !== "GET") {
+            console.log({
+                timestamp: moment.tz("Asia/Jakarta").format(),
+                method: req.method,
+                path: req.path,
+                ip: isIpv6(req.ip) ? convertV6toV4(req.ip) : req.ip,
+                payload: {
+                    body: {
+                        ...req.body
+                    },
+                    query: {
+                        ...req.query
+                    },
+                    params: {
+                        ...req.params
+                    }
                 }
-            }
-        })
+            })
+
+        }
         next();
     }
 
